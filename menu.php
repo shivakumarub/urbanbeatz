87b4adbdec55deba258d396d59035506cc583324
shivakumarub <45612856+shivakumarub@users.noreply.github.com>
2019-01-25 18:43:52 +1000

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,21 @@
+<div class="static-sidebar-wrapper sidebar-midnightblue">
+  <div class="static-sidebar">
+    <div class="sidebar">
+      <div class="widget stay-on-collapse" id="widget-welcomebox">
+        <div class="widget-body welcome-box tabular">
+          <div class="tabular-row">
+            <div class="tabular-cell welcome-options"> <span class="welcome-text">Welcome,</span> <a href="javascript:;" class="name">Admin</a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="widget stay-on-collapse" id="widget-sidebar">
+        <nav role="navigation" class="widget-body">
+          <ul class="acc-menu">
+            <li class="nav-separator"></li>
+            <li><a href="index.php"><i class="fa fa-home"></i><span>Dashboard</span></a></li>
+            <!--<li><a href="javascript:;"><i class="fa fa-columns"></i><span>Master</span><span class="badge badge-primary">3</span></a>
+              <ul class="acc-menu">
+                <li><a href="model.php">Manage Items</a></li>
+                <li><a href="engineer.php">Manage Engineer </a></li>
+                <li><a href="status.php">Manage Status</a></li>
+ </ul>
